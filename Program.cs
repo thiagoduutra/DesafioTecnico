760da2559096cc4ea5491a6caddec52f5c6fd66f
Thiago Dutra <thiagoduutra01@gmail.com>
2026-05-22 21:45:55 -0300

    feat: Realizada questao 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("=== DESAFIO TÉCNICO ===\n");
+
+// ============================================================
+// QUESTÃO 1
+// ============================================================
+Console.WriteLine("--- QUESTÃO 1 ---");
+
+int INDICE = 12, SOMA = 0, K = 1;
+
+while (K < INDICE)
+{
+    K = K + 1;
+    SOMA = SOMA + K;
+}
+
+Imprimir(SOMA);
+
+Console.ReadLine();
+
+static void Imprimir(int valor)
+{
+    Console.WriteLine($"Valor de SOMA: {valor}");
+}
+
+// ============================================================
